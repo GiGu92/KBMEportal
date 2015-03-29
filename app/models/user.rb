4d6fae2112eb39300eb49ce52dd51820bc1c1d3a
Gábor Szalóki <gigu92@gmail.com>
2015-03-29 17:19:56 +0200

    user class dummy data

---
+++ app/models/user.rb
@@ -0,0 +1,8 @@
+class User < ActiveRecord::Base
+  attr_accessor :username
+  attr_accessor :name
+  attr_accessor :email
+  attr_accessor :year
+  attr_accessor :trainingcode
+  attr_accessor :activityoutline
+end
