b9a8e65ef8619f5a4e4a5bc5e943c58b8769ed21
Daniel Ban <ban.daniel.peter@gmail.com>
2015-03-31 13:29:28 +0200

    html button tags replaced with ruby link_to

--- app/controllers/owngroupreports_controller.rb
+++ app/controllers/owngroupreports_controller.rb
@@ -31,17 +31,17 @@ class OwngroupreportsController < ApplicationController
     @report2.circle = 'AC Studio'
     
 
-   #@comment1 = Comment.new
-   #@comment1.comment_text = 'Nagyon szép beszámoló!'
-   #@comment1.date = Time.new
-   #@comment1.sender = 'Bíráló Ferenc'
-
-   #@comment2 = Comment.new
-   #@comment2.comment_text = 'Köszönöm!'
-   #@comment2.date = Time.new
-   #@comment2.sender = 'Tesztelő Aladár'
-
-   #@report1.comments = [@comment1, @comment2]
+    #@comment1 = Comment.new
+    #@comment1.comment_text = 'Nagyon szép beszámoló!'
+    #@comment1.date = Time.new
+    #@comment1.sender = 'Bíráló Ferenc'
+#
+    #@comment2 = Comment.new
+    #@comment2.comment_text = 'Köszönöm!'
+    #@comment2.date = Time.new
+    #@comment2.sender = 'Tesztelő Aladár'
+#
+    #@report1.comments = [@comment1, @comment2]
     @tender1.reports = [@report1, @report2]
     @user1.tender = @tender1
   end
--- config/routes.rb
+++ config/routes.rb
@@ -18,7 +18,7 @@ Rails.application.routes.draw do
   get 'users/update'
   get 'users/profile'
   get 'users/login'
-
+  
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
