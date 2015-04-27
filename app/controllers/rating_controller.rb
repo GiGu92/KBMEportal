e6d4a27db71abcff0ec2b7ca6879bd9cc75e6c20
Gábor Szalóki <gigu92@gmail.com>
2015-04-27 14:02:49 +0200

    rating page partial functionality

--- app/controllers/rating_controller.rb
+++ app/controllers/rating_controller.rb
@@ -47,8 +47,24 @@ class RatingController < ApplicationController
     #@tender3.activity_outline = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
     #@users = [@user1, @user2, @user3]
 
-    @users = User.pluck(:id, :name)
-    @selected_user = User.find(1)
+    @users = User.pluck(:id, :name).sort_by { |user| user[1] }
+    selected_id = request.fullpath.split("userid=")[1]
+    selected_id ||= @users[0][0]
+
+    @previous_index = 0
+    @selected_index = 0
+    @next_index = 1
+    @users.each_with_index do |user, index|
+      if user[0] == selected_id
+        @previous_index = index - 1
+        @selected_index = index
+        @next_index = index + 1
+        @previous_index = 0 if @previous_index < 0
+        @next_index = @users.length - 1 if @next_index >= @users.length
+      end
+    end
+
+    @selected_user = User.find(selected_id)
     @selected_user_reports = @selected_user.tender.reports.sort_by { |report| report.group.name }
   end
 
